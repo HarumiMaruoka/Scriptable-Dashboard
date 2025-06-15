9644817a66658dd8cf62faf516f0be90b22dbc95
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2025-06-15 23:28:27 +0900

    今度こそ終わりは近い

--- Assets/Sample/EnemyDashboardWindow2.cs
+++ Assets/Sample/EnemyDashboardWindow2.cs
@@ -3,12 +3,42 @@ using System;
 using UnityEditor;
 using UnityEngine;
 
-public class EnemyDashboardWindow2 : ScriptableDahsboardEditor<EnemyData>
+public class EnemyDashboardWindow2 : ScriptableDashboardEditor<EnemyData>
 {
-    public static void ShowWindow(ScriptableDashboard<EnemyData> dashboard)
+    public static void ShowWindow(EnemyDashboard dashboard)
     {
-        EnemyDashboardWindow2 wnd = GetWindow<EnemyDashboardWindow2>();
-        wnd.Init(dashboard);
-        wnd.titleContent = new GUIContent("ScriptableDahsboardEditor");
+        EnemyDashboardWindow2 window = GetWindow<EnemyDashboardWindow2>();
+        window.Setup(dashboard);
     }
 }
+
+#if UNITY_EDITOR
+[UnityEditor.CustomEditor(typeof(EnemyDashboard))]
+public class EnemySheetDrawer : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Open Window"))
+        {
+            var dashboard = target as EnemyDashboard;
+            if (dashboard != null)
+            {
+                EnemyDashboardWindow2.ShowWindow(dashboard);
+            }
+        }
+
+        base.OnInspectorGUI();
+    }
+
+    [UnityEditor.Callbacks.OnOpenAsset]
+    public static bool OnOpenAsset(int instanceId, int line)
+    {
+        if (UnityEditor.Selection.activeObject is EnemyDashboard enemyDashboard)
+        {
+            EnemyDashboardWindow2.ShowWindow(enemyDashboard);
+            return true;
+        }
+        return false;
+    }
+}
+#endif
